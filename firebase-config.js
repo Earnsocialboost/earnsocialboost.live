51cc202008a55e2b847c4d50e2401e8668cc4db4
Earnsocialboost <Earnsocialboost@hotmail.com>
2025-06-24 23:52:22 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyBm-uyUX60MFUVr0AZtoZCGAprjfg1uGv8",
+  authDomain: "earn-social-boost.firebaseapp.com",
+  projectId: "earn-social-boost",
+  storageBucket: "earn-social-boost.firebasestorage.app",
+  messagingSenderId: "811427817775",
+  appId: "1:811427817775:web:ad855d010101e3f18c99da",
+  measurementId: "G-DT77418RTG"
+};
